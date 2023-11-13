9b65b8597a0fcc939640eddd4ef06bd3e6d458f7
kailash <kailashrajut0575@gmail.com>
2023-11-13 21:42:54 +0530

    features/banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-
-
 const Home = () => {
+  
   return (
     <div>
-      Home
+      <div className="banner_background   ">
+<h1  className='text-[56px] text-white  mx-auto  text-center py-10' >The #1 site real estate professionals trust*
+</h1>
+<div className="buy_rent_sell_pre_approvel flex list-none gap-3 text-white justify-center py-10 text-[16px]">
+  <li>Buy</li>
+  <li>Rent</li>
+  <li>Sell</li>
+  <li> Pre-approvel</li>
+</div>
+      </div>
     </div>
   )
 }
