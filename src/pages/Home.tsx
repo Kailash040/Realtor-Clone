c6372571bd53750407e6624584cc09325c53562a
kailash <kailashrajut0575@gmail.com>
2023-11-17 20:16:44 +0530

    features/firebase_database

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,34 @@
+import * as React from "react";
+import  { firestore } from '../firebase'
+import  {addDoc , collection} from "@firebase/firestore"
 const Home = () => {
-  return <>insert firebse data here</>;
+  const massageRef = React.useRef<HTMLInputElement>(null);
+  const  ref = collection(firestore ,"messages")
+  const handleSubmit =  (event: React.FormEvent) => {
+  
+    event.preventDefault();
+    // console.log(massageRef.current.value);
+    let  data ={
+      message :  massageRef.current.value
+    }
+    try{
+    addDoc(ref,data)
+    }
+    catch(e){
+      console.log(data)
+    }
+  };
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="massgage">Enter Massage</label>
+          <input type="text" ref={massageRef} />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Home;
