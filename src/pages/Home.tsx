a2a66781473660b267c6462d9a561da11bac8b40
kailash <kailashrajut0575@gmail.com>
2023-11-17 20:40:44 +0530

    features/insert data in firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,25 @@
 import * as React from "react";
-import { firestore } from "../firebase";
-import { addDoc, collection } from "@firebase/firestore";
+import  { firestore } from '../firebase'
+import  {addDoc , collection} from "@firebase/firestore"
 const Home = () => {
   const massageRef = React.useRef<HTMLInputElement>(null);
-  const ref = collection(firestore, "messages");
-  const handleSubmit = (event: React.FormEvent) => {
+  const  ref = collection(firestore ,"messages")
+  const handleSubmit =  (event: React.FormEvent) => {
+  
     event.preventDefault();
     // console.log(massageRef.current.value);
-    let data = {
-      message: massageRef.current.value,
-    };
-    try {
-      addDoc(ref, data);
-    } catch (e) {
-      console.log(data);
+    let  data ={
+      message :  massageRef.current.value
+    }
+    try{
+    addDoc(ref,data)
+    }
+    catch(e){
+      console.log(data)
     }
   };
   return (
-    <div>
+    <>
       <div>
         <form onSubmit={handleSubmit}>
           <label htmlFor="massgage">Enter Massage</label>
@@ -25,7 +27,7 @@ const Home = () => {
           <button type="submit">submit</button>
         </form>
       </div>
-    </div>
+    </>
   );
 };
 
