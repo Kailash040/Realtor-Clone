6bd2bf35d9e2809743ef08863fb7435bb147a572
kailash <kailashrajut0575@gmail.com>
2023-11-20 18:24:18 +0530

    continue working with firebase

--- src/component/Slider.tsx
+++ src/component/Slider.tsx
@@ -9,10 +9,17 @@ const Slider = () => {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide><SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
+      <SwiperSlide>Slide </SwiperSlide>
      <h1>sliderjs</h1>
     </Swiper>
     </div>
