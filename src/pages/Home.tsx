7339c2a41372a8fddc1bc9aacd1d6433c908b97d
kailash <kailashrajut0575@gmail.com>
2023-11-26 17:56:24 +0530

    fetch_firebase_data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { firestore } from "../firebase";
-import { addDoc, collection , getDocs } from "@firebase/firestore";
+import { addDoc, collection, getDocs } from "@firebase/firestore";
 const Home = () => {
   const massageRef = React.useRef<HTMLInputElement>(null);
   const ref = collection(firestore, "messages");
+  // Create firebase Data
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
@@ -12,24 +13,32 @@ const Home = () => {
     };
     try {
       addDoc(ref, data);
-      console.log(data.message)
+      console.log(data.message);
     } catch (e) {
       console.log(data);
     }
   };
   console.log(massageRef.current);
-  React.useEffect(()=>{
-
-    const fetchPost = async ()=>{
-await getDocs(collection(firestore,"messages")).then((querySnapShot)=>{
-const newData = querySnapShot.docs.map((doc)=>(
-  {...doc.data(), id:doc.id}
-))
-console.log(newData)
-})
-    }
+  // Read firebase Data
+  React.useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        await getDocs(collection(firestore, "messages")).then(
+          (querySnapShot) => {
+            const newData = querySnapShot.docs.map((doc) => ({
+              ...doc.data(),
+              id: doc.id,
+            }));
+            // console.log(newData)
+          }
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
     fetchPost();
-  },[])
+  }, []);
+  // 
   return (
     <>
       <div>
