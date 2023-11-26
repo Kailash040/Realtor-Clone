92af9fe65b1f9ba00a44428f4fcc6b902d489fcd
kailash <kailashrajut0575@gmail.com>
2023-11-26 17:48:40 +0530

    fetch_firebase_data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { firestore } from "../firebase";
-import { addDoc, collection } from "@firebase/firestore";
+import { addDoc, collection , getDocs } from "@firebase/firestore";
 const Home = () => {
   const massageRef = React.useRef<HTMLInputElement>(null);
   const ref = collection(firestore, "messages");
@@ -17,7 +17,19 @@ const Home = () => {
       console.log(data);
     }
   };
-  console.log(massageRef.current)
+  console.log(massageRef.current);
+  React.useEffect(()=>{
+
+    const fetchPost = async ()=>{
+await getDocs(collection(firestore,"messages")).then((querySnapShot)=>{
+const newData = querySnapShot.docs.map((doc)=>(
+  {...doc.data(), id:doc.id}
+))
+console.log(newData)
+})
+    }
+    fetchPost();
+  },[])
   return (
     <>
       <div>
