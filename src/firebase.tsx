c6bdcce760bc22920fb854a9c4e2f85423d53e83
kailash <kailashrajut0575@gmail.com>
2023-11-26 21:20:50 +0530

    signUp_page

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import  { getFirestore } from '@firebase/firestore'
-
+import { getAuth } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyBFkJDyUo0JQ43z4YEyfuivxZyWzLR-F9c",
   authDomain: "realoneclone.firebaseapp.com",
@@ -15,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export  const  firestore  =getFirestore(app);
+export const auth = getAuth(app)
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,8 +1,87 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import  {useState} from 'react';
+import  {NavLink ,useNavigate} from "react-router-dom";
+import  {auth} from '../firebase';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+ ///// SignUp functionality ////////////////
+//  
+const onSubmit = async (e) => {
+  e.preventDefault()
+ 
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/login")
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+    });
+
+
+}
   return (
     <div>
-      
+       <main >        
+        <section>
+            <div>
+                <div>                  
+                    <h1> FocusApp </h1>                                                                            
+                    <form>                                                                                            
+                        <div>
+                            <label htmlFor="email-address">
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}  
+                                required                                    
+                                placeholder="Email address"                                
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required                                 
+                                placeholder="Password"              
+                            />
+                        </div>                                             
+                        
+                        <button
+                            type="submit" 
+                            onClick={onSubmit}                        
+                        >  
+                            Sign up                                
+                        </button>
+                                                                     
+                    </form>
+                   
+                    <p>
+                        Already have an account?{' '}
+                        <NavLink to="/login" >
+                            Sign in
+                        </NavLink>
+                    </p>                   
+                </div>
+            </div>
+        </section>
+    </main>
     </div>
   )
 }
