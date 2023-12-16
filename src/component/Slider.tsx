81216255f1eb025d2c70c0bc1111852f6ee24e17
kailash <kailashrajut0575@gmail.com>
2023-12-16 20:49:00 +0530

    Wc-1

--- src/component/Slider.tsx
+++ src/component/Slider.tsx
@@ -1,25 +1,24 @@
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-const Slider = () => {
-  return (
-    <div>
- <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-      <SwiperSlide>Slide </SwiperSlide>
-    </Swiper>
-    </div>
-  )
-}
 
-export default Slider
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import '../App.css'
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+export default function App() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide><img src="https://images.lawpath.com.au/2019/07/stencil.blog-post-feature-15.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvcGVydHl8ZW58MHx8MHx8fDA%3D" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?cs=srgb&dl=pexels-binyamin-mellish-1396122.jpg&fm=jpg" alt="" /></SwiperSlide>
+        
+      </Swiper>
+    </>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Header from "../component/Header";
 import Navbar from "../component/Navbar";
 import Slider from "../component/Slider";
-
 const Home = () => {
 
   return (
