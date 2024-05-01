95855ca21ef04c7284b7d2d2e1d797b4e9dda8dd
Kailash Tanwar <kailashrajut0575@gmail.com>
2024-05-01 23:12:16 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/" element={<SignIn />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
